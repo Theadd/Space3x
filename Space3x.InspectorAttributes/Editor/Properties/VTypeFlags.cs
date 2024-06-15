19d0da80dd269d4ea461d59afaac071b9406144b
R. Beltran <rbeltran8000@gmail.com>
2024-06-15 20:30:21 +0200

    move VTypeFlags

---
+++ Space3x.InspectorAttributes/Editor/Properties/VTypeFlags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Space3x.InspectorAttributes.Editor
+{
+    [Flags]
+    public enum VTypeFlags
+    {
+        None = 0,
+        HideInInspector = 1,
+        ShowInInspector = 2,
+        Serializable = 4,
+        NonReorderable = 8,
+    }
+}
