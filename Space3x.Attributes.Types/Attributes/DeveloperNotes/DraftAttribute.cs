27b1e6758ea23a64fbee81ddd6e69cfb030a0de8
R. Beltran <rbeltran8000@gmail.com>
2024-07-15 02:39:24 +0200

    add DeveloperNotes attributes

---
+++ Space3x.Attributes.Types/Attributes/DeveloperNotes/DraftAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Space3x.Attributes.Types.DeveloperNotes
+{
+    /// <summary>
+    /// Types with this attribute are marked as drafts and not production ready.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class DraftAttribute : Attribute { }
+}
