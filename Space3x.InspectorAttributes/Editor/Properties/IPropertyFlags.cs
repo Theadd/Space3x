c93d66df895fa4d150d0770a12e3430c3f685119
R. Beltran <rbeltran8000@gmail.com>
2024-07-26 15:26:50 +0200

    add IsArray(), IsList() and xml doc comments on IPropertyFlags

--- Space3x.InspectorAttributes/Editor/Properties/IPropertyFlags.cs
+++ Space3x.InspectorAttributes/Editor/Properties/IPropertyFlags.cs
@@ -1,46 +1,71 @@
-﻿using JetBrains.Annotations;
-using Space3x.Attributes.Types;
+﻿using Space3x.Attributes.Types;
 
 namespace Space3x.InspectorAttributes.Editor
 {
     /// <summary>
-    /// Flags for <see cref="IPropertyNode"/> and <see cref="Space3x.InspectorAttributes.Editor.VisualElements.BindablePropertyField"/>.
+    /// Flags for <see cref="IPropertyNode"/>.
     /// </summary>
     public interface IPropertyFlags
     {
-        [UsedImplicitly]
         public VTypeFlags Flags { get; }
         
         /// <summary>
-        /// Serializable properties are not hidden, even if they are not public (for example, using SerializeField
-        /// on a private field). But non serialized ones, such as private fields or using the [NonSerialized] attribute,
-        /// are flagged as hidden. The [HideInInspector] and
-        /// <see cref="ShowInInspectorAttribute">[ShowInInspector]</see> attributes also apply here.
+        /// Serializable properties are not hidden, even if they are not public (for example,
+        /// using <see cref="UnityEngine.SerializeField"/> on a private field). But non serialized ones, such as
+        /// private fields or using the <see cref="System.NonSerializedAttribute">NonSerialized</see> attribute,
+        /// are flagged as hidden.
         /// </summary>
-        [UsedImplicitly]
+        /// <remarks>
+        /// The <see cref="UnityEngine.HideInInspector">HideInInspector</see> and
+        /// <see cref="ShowInInspectorAttribute">ShowInInspector</see> attributes also apply here.
+        /// </remarks>
         public bool IsHidden => (Flags | VTypeFlags.HideInInspector) == Flags ||
                                 !((Flags | VTypeFlags.Serializable) == Flags ||
                                   (Flags | VTypeFlags.ShowInInspector) == Flags);
         
+        // /// <summary>
+        // /// Only the most basic information is extracted using reflection when accessing the node tree for the first
+        // /// time on any given object type, in order to determine the property nodes automatically handled by Unity (1)
+        // /// from those that won't be included in the tree (2) unless they're properly annotated (3).
+        // /// </summary>
+        // /// <remarks>
+        // /// 1 - Serializable nodes are fully handled by Unity, populating SerializedProperties, SerializedObjects, 
+        // /// PropertyDrawers, PropertyDecorators, and so on. Those are the ones where IsSerializable is true.
+        // /// 2 - The ones matching: <c>!IsSerializable && IsHidden</c>.
+        // /// 3 - The rest, matching: <c>!IsSerializable && !IsHidden</c>.
+        // /// </remarks>
+        // public bool IsSerializable => (Flags | VTypeFlags.Serializable) == Flags;
+        
         /// <summary>
+        /// Properties that are marked with the <see cref="System.SerializableAttribute">Serializable</see> attribute.
         /// Only the most basic information is extracted using reflection when accessing the node tree for the first
-        /// time on any given object type, in order to determine the property nodes automatically handled by Unity (1)
-        /// from those that won't be included in the tree (2) unless they're properly annotated (3).
-        ///
-        /// (1): Serializable nodes are fully handled by Unity, populating SerializedProperties, SerializedObjects, 
+        /// time on any given object type, in order to determine the property nodes automatically handled by Unity [^1]
+        /// from those that won't be included in the tree [^2] unless they're properly annotated [^3].
+        /// </summary>
+        /// <remarks>
+        /// [^1]: Serializable nodes are fully handled by Unity, populating SerializedProperties, SerializedObjects, 
         /// PropertyDrawers, PropertyDecorators, and so on. Those are the ones where IsSerializable is true.
         /// 
-        /// (2): The ones matching: !IsSerializable && IsHidden.
+        /// [^2]: The ones matching: <c>!IsSerializable &amp;&amp; IsHidden</c>.
         /// 
-        /// (3): The rest, matching: !IsSerializable && !IsHidden.
-        /// </summary>
-        [UsedImplicitly]
+        /// [^3]: The rest, matching: <c>!IsSerializable &amp;&amp; !IsHidden</c>.
+        /// </remarks>
         public bool IsSerializable => (Flags | VTypeFlags.Serializable) == Flags;
         
         /// <summary>
-        /// Makes elements of array or list properties, non-reorderable.
+        /// Makes elements of array or list properties, non-reorderable. See the
+        /// <see cref="UnityEngine.NonReorderableAttribute">NonReorderable</see> attribute.
         /// </summary>
-        [UsedImplicitly]
         public bool IsNonReorderable => (Flags | VTypeFlags.NonReorderable) == Flags;
+        
+        /// <summary>
+        /// Properties that are arrays.
+        /// </summary>
+        public bool IsArray => (Flags | VTypeFlags.Array) == Flags;
+        
+        /// <summary>
+        /// Properties that derive from <see cref="System.Collections.IList">IList</see>.
+        /// </summary>
+        public bool IsList => (Flags | VTypeFlags.List) == Flags;
     }
 }
