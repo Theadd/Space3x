c344345746a22bc3ec9a981642845f3ea12be954
R. Beltran <rbeltran8000@gmail.com>
2024-07-26 15:24:59 +0200

    add SpaceAttribute decorator

---
+++ Space3x.InspectorAttributes/Editor/Drawers/SpaceDecorator.cs
@@ -0,0 +1,26 @@
+﻿using Space3x.InspectorAttributes.Editor.VisualElements;
+using Space3x.UiToolkit.Types;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Space3x.InspectorAttributes.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof(SpaceAttribute), useForChildren: false)]
+    public class SpaceDecorator : Decorator<BlockDecorator, SpaceAttribute>, IAttributeExtensionContext<SpaceAttribute>
+    {
+        public override SpaceAttribute Target => (SpaceAttribute) attribute;
+
+        protected override void OnCreatePropertyGUI(VisualElement container)
+        {
+            var spacer = new VisualElement().WithClasses("ui3x-space");
+            spacer.style.height = Target.height;
+            spacer.style.width = Target.height;
+            container.style.flexGrow = 0f;
+            container.style.flexShrink = 0f;
+            spacer.style.flexGrow = 0f;
+            spacer.style.flexGrow = 0f;
+            container.Add(spacer);
+        }
+    }
+}
