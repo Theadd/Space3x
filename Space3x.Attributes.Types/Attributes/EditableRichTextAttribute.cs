9610a6a4334cf99600b4ba001db24d040d1f10d3
R. Beltran <rbeltran8000@gmail.com>
2024-08-10 00:25:30 +0200

    add EditableRichText attribute

---
+++ Space3x.Attributes.Types/Attributes/EditableRichTextAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Space3x.Attributes.Types
+{
+    /// <summary>
+    /// Displays a string property as an editable Rich Text Label on double-click.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class EditableRichTextAttribute : PropertyAttribute
+    {
+        public EditableRichTextAttribute() : base(applyToCollection: false) { }
+    }
+}
