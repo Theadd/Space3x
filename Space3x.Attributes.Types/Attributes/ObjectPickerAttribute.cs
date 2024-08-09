e5de7777e677d1bd98fb3948480182e948d1e78a
R. Beltran <rbeltran8000@gmail.com>
2024-08-10 00:25:58 +0200

    add ObjectPicker attribute

---
+++ Space3x.Attributes.Types/Attributes/ObjectPickerAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Space3x.Attributes.Types
+{
+    /// <summary>
+    /// Displays an UnityEngine.Object picker.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class ObjectPickerAttribute : PropertyAttribute
+    {
+        public Type ObjectType { get; set; } = typeof(UnityEngine.Object);
+
+        public ObjectPickerAttribute(Type type) : base(applyToCollection: false)
+        {
+            ObjectType = type;
+        }
+        
+        public ObjectPickerAttribute() : base(applyToCollection: false) { }
+    }
+}
