9d4501eaecfb9e8ebf4294d94d5417d849e56da8
R. Beltran <rbeltran8000@gmail.com>
2024-08-10 00:27:10 +0200

    add ObjectPicker drawer

---
+++ Space3x.InspectorAttributes/Editor/Drawers/ObjectPickerDrawer.cs
@@ -0,0 +1,24 @@
+﻿using Space3x.Attributes.Types;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Space3x.InspectorAttributes.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof(ObjectPickerAttribute), useForChildren: true)]
+    public class ObjectPickerDrawer : Drawer<ObjectPickerAttribute>
+    {
+        public override ObjectPickerAttribute Target => (ObjectPickerAttribute) attribute;
+        
+        protected override VisualElement OnCreatePropertyGUI(IPropertyNode property)
+        {
+            var field = new UnityEditor.Search.ObjectField()
+            {
+                label = property.DisplayName(),
+                objectType = Target.ObjectType
+            };
+            field.AddToClassList(BaseField<string>.alignedFieldUssClassName);
+            field.BindProperty(property);
+            return field;
+        }
+    }
+}
