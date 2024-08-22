13a5238b3e9a8fb66e2759e33ea921bdb73ed163
R. Beltran <rbeltran8000@gmail.com>
2024-08-22 02:23:38 +0200

    add IPropertyController

---
+++ Space3x.InspectorAttributes/Editor/Properties/IPropertyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace Space3x.InspectorAttributes.Editor
+{
+    public interface IPropertyController
+    {
+        public string ParentPath { get; }
+        
+        public bool IsSerialized { get; }
+        
+        public SerializedObject SerializedObject { get; }
+
+        public IPropertyNode GetProperty(string propertyName);
+
+        public IPropertyNode GetProperty(string propertyName, int arrayIndex);
+    }
+}
