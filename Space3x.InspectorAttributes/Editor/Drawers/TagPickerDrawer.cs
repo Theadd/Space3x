4d26fe1a56ea744ace5221dfee1085c316208fe3
R. Beltran <rbeltran8000@gmail.com>
2024-08-22 02:22:12 +0200

    add TagPickerDrawer

---
+++ Space3x.InspectorAttributes/Editor/Drawers/TagPickerDrawer.cs
@@ -0,0 +1,19 @@
+﻿using Space3x.Attributes.Types;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Space3x.InspectorAttributes.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof(TagPickerAttribute), useForChildren: true)]
+    public class TagPickerDrawer : Drawer<TagPickerAttribute>
+    {
+        protected override VisualElement OnCreatePropertyGUI(IPropertyNode property)
+        {
+            var field = new TagField() { label = preferredLabel };
+            field.AddToClassList(BaseField<string>.alignedFieldUssClassName);
+            field.BindProperty(property);
+            return field;
+        }
+    }
+}
