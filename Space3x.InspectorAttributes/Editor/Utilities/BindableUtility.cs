a3ac778773f56de93ee774c172750fd7c509876a
R. Beltran <rbeltran8000@gmail.com>
2024-08-28 22:01:18 +0200

    update utilities

--- Space3x.InspectorAttributes/Editor/Utilities/BindableUtility.cs
+++ Space3x.InspectorAttributes/Editor/Utilities/BindableUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using UnityEngine;
 using UnityEngine.Internal;
 using UnityEngine.UIElements;
 
@@ -28,11 +29,6 @@ namespace Space3x.InspectorAttributes.Editor.Utilities
             {
                 control.RegisterValueChangedCallback<T>(ev =>
                 {
-                    // if (element.dataSource is BindableDataSource<T> bindableSource)
-                    // {
-                    //     if (RuntimeHelpers.Equals(bindableSource.Value, ev.newValue) && !RuntimeHelpers.Equals(ev.previousValue, ev.newValue))
-                    //         bindableSource.NotifyValueChanged();
-                    // }
                     if (element.dataSource is DataSourceBinding sourceBinding)
                     {
                         if (RuntimeHelpers.Equals((T) sourceBinding.Value, ev.newValue) && !RuntimeHelpers.Equals(ev.previousValue, ev.newValue))
@@ -40,7 +36,7 @@ namespace Space3x.InspectorAttributes.Editor.Utilities
                     }
                     else
                     {
-                        property.NotifyValueChanged();
+                        property.NotifyValueChanged(property);
                     }
                 });
             }
@@ -62,6 +58,24 @@ namespace Space3x.InspectorAttributes.Editor.Utilities
         {
             if (element is INotifyValueChanged<T> control) 
                 control.RegisterValueChangedCallback((_) => callback());
+        }
+        
+        public static void SetValueWithoutNotify(VisualElement element, object value)
+        {
+            object gValue = (
+                    element.GetType().GetProperty("value", BindingFlags.Public | BindingFlags.Instance)
+                    ?? element.GetType().GetProperty("Value", BindingFlags.Public | BindingFlags.Instance)
+                )
+                ?.GetValue(element);
+            if (gValue == null) return;
+            dynamic dynValue = gValue;
+            SetValueWithoutNotifyWrapper(dynValue, element, value);
+        }
+        
+        private static void SetValueWithoutNotifyWrapper<T>(T dynamicValue, VisualElement element, object value)
+        {
+            if (element is INotifyValueChanged<T> control) 
+                control.SetValueWithoutNotify((T) value);
         }
     }
 }
