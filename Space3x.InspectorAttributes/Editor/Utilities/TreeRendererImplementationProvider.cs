fb3e03c82fa7ff2463861ba0683990897af460e4
R. Beltran <rbeltran8000@gmail.com>
2024-09-11 06:36:29 +0200

    add TreeRenderer utility default implementation

---
+++ Space3x.InspectorAttributes/Editor/Utilities/TreeRendererImplementationProvider.cs
@@ -0,0 +1,23 @@
+﻿using Space3x.InspectorAttributes.Editor.VisualElements;
+using Space3x.Properties.Types;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Space3x.InspectorAttributes.Editor
+{
+    [InitializeOnLoad]
+    internal class TreeRendererImplementationProvider : ITreeRendererUtility
+    {
+        static TreeRendererImplementationProvider() =>
+            TreeRendererUtility.RegisterImplementationProvider(new TreeRendererImplementationProvider());
+        
+        public VisualElement Create(IPropertyNode property)
+        {
+            return new FoldoutTreeRenderer()
+            {
+                text = property.DisplayName(),
+                value = true
+            };
+        }
+    }
+}
