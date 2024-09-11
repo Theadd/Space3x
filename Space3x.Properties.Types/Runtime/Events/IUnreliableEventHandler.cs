6ec93a12afe6be17cc4b04eac014df15ffdd2d90
R. Beltran <rbeltran8000@gmail.com>
2024-09-11 06:29:32 +0200

    refactoring

---
+++ Space3x.Properties.Types/Runtime/Events/IUnreliableEventHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Space3x.Properties.Types
+{
+    /// <summary>
+    /// It is a non-standard event handler custom implementation only for a very specific subset of the property tree
+    /// and provides change event propagation on controllers (all properties on the same declaring object share a single
+    /// controller instance) descending from elements in a non-serialized array or list, down to the closest property
+    /// for an <see cref="UnityEngine.Object"/> derived type.    
+    /// </summary>
+    public interface IUnreliableEventHandler
+    {
+        IBindablePropertyNode SourcePropertyNode { get; }
+
+        bool IsTopLevelRuntimeEventHandler { get; }
+
+        IUnreliableEventHandler GetTopLevelEventHandler();
+
+        IBindablePropertyNode GetReliablePropertyNode();
+        
+        void NotifyValueChanged(IBindablePropertyNode propertyNode);
+    }
+}
